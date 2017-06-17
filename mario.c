75db93afb663a43c9715627d98204ae32cf61249
BenDeb <bendebsx@gmail.com>
2017-06-17 17:54:19 +0200

    ajout prise en charge demi-pyramide droite

--- mario.c
+++ mario.c
@@ -1,12 +1,9 @@
-/* Output :
-
-entrez une taille pour la pyramide : 6
-     ##
-    ###
-   ####
-  #####
- ######
-####### */
+/*entrez une taille pour la pyramide : 5
+    #  #
+   ##  ##
+  ###  ###
+ ####  ####
+#####  #####*/
 
 #include <stdio.h>
 #include <cs50.h>
@@ -14,9 +11,9 @@ void pyramid(void);
 
 int main(void)
 {
-
+    
     pyramid();
-
+    
 }
 
 void pyramid(void)
@@ -27,24 +24,24 @@ void pyramid(void)
         printf("entrez une taille pour la pyramide : ");
         x = get_int();
     }
-    /* la taille de la pyramide ne doit pas excéder 23 ou être
-    inférieure à 0 */
     while (x > 23 || x < 0);
     /*première boucle, on boucle n fois*/
     for (int j = 0; j < x; j++)
     {
-        /*1ère boucle intérieure, r = x, on boucle tant que r est supérieur à j + 1, on enlève 1 à r à chaque tour de boucle,
-        cela signifie qu'à chaque tour de boucle on affiche un espace en moins
-        */
         for (int r = x; r > j + 1; r--)
         {
             printf(" ");
         }
-        /* 2ème boucle, on boucle tant que i - 1 < j + 1, et on ajoute 1 à i, donc à chaque tour de boucle on ajoute un # */
-        for (int i = 0; i - 1 < j + 1; i++)
+        for (int i = 0; i < j + 1; i++)
         {
             printf("#");
         }
-        printf("\n");
+        printf("  ");
+        for (int k = 0; k < j + 1; k++)
+        {
+            printf("#");
         }
+        
+        printf("\n");
     }
+}
